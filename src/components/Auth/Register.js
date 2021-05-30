cbf659f9a8d682edda755395f85b07288fe555ce
Sourya Kaushik <sourya.kaushik@hashedin.com>
2021-05-30 15:15:20 +0530

    added the register form validation

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,5 +1,5 @@
 import React from "react";
-import firebase from '../../firebase'
+import firebase from "../../firebase";
 import {
   Grid,
   Form,
@@ -13,33 +13,74 @@ import { Link } from "react-router-dom";
 
 class Register extends React.Component {
   state = {
-    username:"",
-    email:"",
-    password:"",
-    passwordConfirmation:""
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
+    errors:[]
   };
+
+  isFormValid = () =>{
+    let errors=[]
+    let error;
+     if(this.isFormEmpty(this.state)){
+       error={Message :'Fill in all fields'};
+       this.setState({errors : errors.concat(error)});
+       return false;
+
+     }
+
+     else if(!this.isPasswordValid(this.state)){
+       error ={Message : "Password is invalid"}
+       this.setState({errors : errors.concat(error)});
+       return false;
+
+     }
+     else{
+       return true;
+     }
+  }
+
+  isFormEmpty = ({username,email,password,passwordConfirmation}) =>{
+    return !username.length || !email.length || !password.length
+    || !passwordConfirmation.length;
+
+  }
+
+  isPasswordValid = ({password,passwordConfirmation}) => {
+    if(password.length < 6 || passwordConfirmation.length <6){
+      return false;
+    }
+    else if(password !== passwordConfirmation){
+      return false;
+    }
+    return true;
+  }
+
+  displayErrors = errors => errors.map((error,i) => <p keys={i}>{error.Message}</p>)
 
   handleChange = event => {
-    this.setState({[event.target.name] : event.target.value})
+    this.setState({ [event.target.name]: event.target.value });
   };
 
-  handleSubmit = event =>{
-    event.preventDefault();
-
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .them(createdUser => {
-        console.log(createdUser);
-      })
-      .catch(err =>{
-        console.log(err);
-      });
+  handleSubmit = event => {
+    if(this.isFormValid()){
+      event.preventDefault();
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(createdUser => {
+          console.log(createdUser);
+        })
+        .catch(err => {
+          console.error(err);
+        });
+    }  
   };
 
   render() {
+    const { username, email, password, passwordConfirmation,errors } = this.state;
 
-    const { username , email , password , passwordConfirmation}=  this.state;
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -98,6 +139,12 @@ class Register extends React.Component {
               </Button>
             </Segment>
           </Form>
+          {errors.length > 0 &&(
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(errors)}
+            </Message>
+          )}
           <Message>
             Already a user? <Link to="/login">Login</Link>
           </Message>
--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import "firebase/database";
 import "firebase/storage";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDBqYta2cUfyqp6rnIPedMWhX4a1cfcBt8",
-    authDomain: "slack-1dbfe.firebaseapp.com",
-    projectId: "slack-1dbfe",
-    storageBucket: "slack-1dbfe.appspot.com",
-    messagingSenderId: "809505581351",
-    appId: "1:809505581351:web:2e10cb575883cb09a66102"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyDBqYta2cUfyqp6rnIPedMWhX4a1cfcBt8",
+  authDomain: "slack-1dbfe.firebaseapp.com",
+  projectId: "slack-1dbfe",
+  storageBucket: "slack-1dbfe.appspot.com",
+  messagingSenderId: "809505581351",
+  appId: "1:809505581351:web:2e10cb575883cb09a66102"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;
